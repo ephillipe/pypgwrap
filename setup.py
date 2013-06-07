8852ac18e8200d31534db34328ab21149a189499
Erick Almeida <ephillipe@gmail.com>
2013-06-07 09:43:17 -0300

    Updating setup.py for register with PIP tool

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 version = "0.1"
 description = 'PostgreSQL database wrapper - provides wrapper over psycopg2 supporting a Python API for common sql ' \
               'functions, transaction and pooling'
-long_description = file("README.md").read()
+long_description = file("README").read()
 
 
 class GenerateReadme(Command):
@@ -24,15 +24,19 @@ class GenerateReadme(Command):
     def run(self):
         import pypgwrap,textwrap
         long_description = textwrap.dedent(pypgwrap.__doc__)
-        open("README.md","w").write(long_description)
+        open("README", "w").write(long_description)
 
 setup(name='pypgwrap',
       version = version,
       description = description,
       long_description = long_description,
+      platforms=["any"],
       author = 'Erick Almeida',
       author_email = 'ephillipe@gmail.com',
+      maintainer="Erick Almeida",
+      maintainer_email="ephillipe@gmail.com",
       url = 'https://github.com/ephillipe/pypgwrap',
+      download_url = 'https://pypi.python.org/packages/source/p/pypgwrap/pypgwrap-0.1.zip#md5=2a9eaf73706d1242a84d63f8dfd1ed46',
       cmdclass = { 'readme' : GenerateReadme },
       packages = ['pypgwrap'],
       install_requires = ['psycopg2'],
